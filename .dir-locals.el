bd7d0179212939fdef42006c328a8fef589735a1
Omar Polo <op@omarpolo.com>
2021-09-30 10:02:26 +0000

    improve dir-locals
    force tabs on and use c-file-offset instead of c-offsets-alist because
    it's meant to be used in dir-locals.

--- .dir-locals.el
+++ .dir-locals.el
@@ -1,7 +1,8 @@
 ;;; Directory Local Variables
 ;;; For more information see (info "(emacs) Directory Variables")
 
-((c-mode . ((c-default-style . "K&R")
+((c-mode . ((indent-tabs-mode . t)
+	    (c-default-style . "K&R")
 	    (c-basic-offset . 8)
-	    (c-offsets-alist . ((arglist-intro . +)
-				(arglist-cont-nonempty . *))))))
+	    (c-file-offsets . ((arglist-intro . +)
+			       (arglist-cont-nonempty . *))))))
